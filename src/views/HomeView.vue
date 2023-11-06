eaa217b65bda0d4f50361de36ee597e6758f8e4d
Hazim Bakar <119983226+hazimba@users.noreply.github.com>
2023-11-06 16:32:22 +0800

    Add files via upload

---
+++ src/views/HomeView.vue
@@ -0,0 +1,21 @@
+<template>
+  <b-container>
+    <div class="modal" :id="modal.modalClientId" v-for="employee in modal.data" :key="employee.id" title="employee data" @click.stop>
+      <div class="modal-content" v-if="show">
+        <div>{{ modal.data.profile_image }}</div>
+      </div>
+    </div>
+  </b-container>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'HomeView',
+  components: {
+    HelloWorld
+  }
+}
+</script>
